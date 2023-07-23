93190d40f1e3aa715c7e05adca222d152915f722
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-07-22 22:34:51 -0400

    remove callbacks from auth requests

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { User } from "src/types/payload-types";
 
 type Login = (args: { email: string; password: string }) => Promise<void>;
@@ -24,55 +18,63 @@ export const AuthProvider: React.FC<React.PropsWithChildren<{}>> = ({
 }) => {
   const [user, setUser] = useState<User | null>();
 
-  const login = useCallback<Login>(async (args) => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}/api/users/login`,
-      {
-        method: "POST",
-        body: JSON.stringify(args),
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    if (response.ok) {
-      const json = await response.json();
-      setUser(json.user);
-    } else {
-      throw new Error("Invalid login");
-    }
-  }, []);
-
-  const logout = useCallback(async () => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}/api/users/logout`,
-      {
-        method: "POST",
-        credentials: "include",
-      }
-    );
-
-    if (response.ok) {
-      setUser(null);
-    } else {
-      throw new Error("There was an problem while logging out.");
-    }
-  }, []);
-
-  useEffect(() => {
-    const fetchUser = async () => {
+  const login: Login = async (args) => {
+    try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}/api/users/me`,
+        `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}/api/users/login`,
         {
+          method: "POST",
+          body: JSON.stringify(args),
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+        setUser(json.user);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}/api/users/logout`,
+        {
+          method: "POST",
           credentials: "include",
         }
       );
 
       if (response.ok) {
-        const result = await response.json();
-        setUser(result.user);
+        setUser(null);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}/api/users/me`,
+          {
+            credentials: "include",
+          }
+        );
+
+        if (response.ok) {
+          const result = await response.json();
+          setUser(result.user);
+        }
+      } catch (e) {
+        console.error(e);
       }
     };
 
